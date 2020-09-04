ec15c6a7b75a4f8422aebf355e979b19050b45f7
Mariana Barros <mamirandabarros11@gmail.com>
2020-09-04 09:40:07 -0300

    home style

--- src/Pages/Header/index.js
+++ src/Pages/Header/index.js
@@ -17,7 +17,7 @@ export default function () {
           <span onClick={onClickPokemons}>Login</span>
         </div>
       </nav>
-      <a><img class="title" src="https://fontmeme.com/permalink/200429/65da7acdb7f16431618ddbf607e03edc.png"
+      <a><img class="title" src="https://fontmeme.com/permalink/200904/4694367609be71dac48b393fda107914.png"
         alt="fonte-de-pokemon-go" border="0" /></a>
     </>
   )
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -2,22 +2,20 @@ import React, { useEffect } from 'react';
 import './style.css';
 import '../../Gallery/gallery.theme.css';
 import '../../Gallery/gallery.min.css';
+import Header from "../Header/index";
 
 export default function () {
   return (
     <>
-      <header class="ola">
+      <header >
+        <Header />
         <section class="gallery autoplay items-3">
           <div id="item-1" class="control-operator"></div>
           <div id="item-2" class="control-operator"></div>
           <div id="item-3" class="control-operator"></div>
-
           <figure class="item">
             <div>
-              <a>
-                <img class="top" src="https://fontmeme.com/permalink/200429/196ada5df6b06cbbd0b9ee6f014559b4.png"
-                  alt="fonte-de-pokemon-go" border="0" />
-              </a>
+              <span class="top">Top 3 - Ataque</span>
             </div>
             <a href="./detail?num=150"><img class="img-top3" src="http://www.serebii.net/pokemongo/pokemon/150.png" /></a>
             <a href="./detail?num=065"><img class="img-top3" src="http://www.serebii.net/pokemongo/pokemon/065.png" /></a>
@@ -26,10 +24,7 @@ export default function () {
 
           <figure class="item">
             <div>
-              <a>
-                <img class="top" src="https://fontmeme.com/permalink/200429/8aac5fd1cefbdfee2d39542be186b792.png"
-                  alt="fonte-de-pokemon-go" border="0" />
-              </a>
+              <span class="top">Top 3 - Defesa</span>
             </div>
             <a href="./detail?num=091"><img class="img-top3" src="http://www.serebii.net/pokemongo/pokemon/091.png" /></a>
             <a href="./detail?num=095"><img class="img-top3" src="http://www.serebii.net/pokemongo/pokemon/095.png" /></a>
@@ -38,10 +33,7 @@ export default function () {
 
           <figure class="item">
             <div>
-              <a>
-                <img class="top" src="https://fontmeme.com/permalink/200429/0a4dce5e5599f1eee1fd33563b17a86c.png"
-                  alt="fonte-de-pokemon-go" border="0" />
-              </a>
+              <span class="top">Top 3 - HP</span>
             </div>
             <a href="./detail?num=113"><img class="img-top3" src="http://www.serebii.net/pokemongo/pokemon/113.png" /></a>
             <a href="./detail?num=143"><img class="img-top3" src="http://www.serebii.net/pokemongo/pokemon/143.png" /></a>
@@ -55,18 +47,18 @@ export default function () {
           </div>
         </section>
       </header>
-      <div>
+      <div class="title-home">
         <span class="sub-title">Descubra o Universo Pokemon!!!</span>
-        <span>
-          <p>O que são Pokémon?</p>
-          Pokémon são criaturas de todas as formas e tamanhos que convivem com os humanos na natureza.
+        <div class="text-pokemon">
+          <h1>O que são Pokémon?</h1>
+          <p>Pokémon são criaturas de todas as formas e tamanhos que convivem com os humanos na natureza.
           Na grande maioria, os Pokémon não falam, exceto para proferir seus nomes.
           Os Pokémon são criados e comandados por seus donos (os chamados "Treinadores").
           No decorrer das aventuras, os Pokémon crescem e ganham experiência, podendo até mesmo evoluir para
           Pokémon mais fortes. Alguns Pokémon, como Pikachu, Piplup e Charizard, possuem papéis de destaque na
           série de videogames, no jogo Estampas Ilustradas e nos programas de TV, mas eles são apenas algumas
-          das quase 500 criaturas que habitam o universo dos Pokémon.
-        </span>
+          das quase 500 criaturas que habitam o universo dos Pokémon.</p>
+        </div>
 
       </div>
     </>
