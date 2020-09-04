fa49e58a5d421ae7cc1da294bf3bb812ed53a0d8
Mariana Barros <mamirandabarros11@gmail.com>
2020-09-04 11:24:47 -0300

    style

--- src/Pages/Header/index.js
+++ src/Pages/Header/index.js
@@ -10,15 +10,17 @@ export default function () {
   };
   return (
     <>
-      <nav>
-        <div>
-          <span onClick={onClickPokemons}>Pagina Inicial</span>
-          <span onClick={onClickPokemons}>Pokemons</span>
-          <span onClick={onClickPokemons}>Login</span>
-        </div>
-      </nav>
-      <span><img className="title" src="https://fontmeme.com/permalink/200904/4694367609be71dac48b393fda107914.png"
-        alt="fonte-de-pokemon-go" border="0" /></span>
+      <div className="header">
+        <nav>
+          <div>
+            <span onClick={onClickPokemons}>Pagina Inicial</span>
+            <span onClick={onClickPokemons}>Pokemons</span>
+            <span onClick={onClickPokemons}>Login</span>
+          </div>
+        </nav>
+        <span><img className="title" src="https://fontmeme.com/permalink/200904/4694367609be71dac48b393fda107914.png"
+          alt="fonte-de-pokemon-go" border="0" /></span>
+      </div>
     </>
   )
 }
