2b34d54e906fe65d1ea2765222bd71b3f562cc8e
Mariana Barros <mamirandabarros11@gmail.com>
2020-09-07 12:42:42 -0300

    Updates

---
+++ precache-manifest.9c053333310efe648a42e4a8efced6b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce36e75d2760cb7499b75c9b8ab4b01c",
+    "url": "/pokemosdata/index.html"
+  },
+  {
+    "revision": "0f4f5159fffd6088aa62",
+    "url": "/pokemosdata/static/css/main.963d3eb4.chunk.css"
+  },
+  {
+    "revision": "6a99eea32c93bb3d9156",
+    "url": "/pokemosdata/static/js/2.84c816fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokemosdata/static/js/2.84c816fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f4f5159fffd6088aa62",
+    "url": "/pokemosdata/static/js/main.458cf28e.chunk.js"
+  },
+  {
+    "revision": "b2d5b0e774434d55d0a7",
+    "url": "/pokemosdata/static/js/runtime-main.68cd8379.js"
+  }
+]);
